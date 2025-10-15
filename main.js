7e9034739887b299ea73bf002692bd54fe7724e8
AlbertProfeIronhack <albert.vandellos@ironhack.com>
2025-10-15 20:30:01 +0200

    añadidos comentarios JSdoc

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const titulo = document.getElementById("titular");
 
-
+/** funcion que cambia el texto en el DOM
+ * esto ese si hubiera params y return:
+ * @param {string} textCLiente - el texto que va a sustituir el anterior
+ * @return {void} - no retorna nada, cambia el DOM
+ */
 function cambiarTexto(){
     titulo.innerHTML = "Texto cambiado!";
 }
@@ -9,7 +13,11 @@ const numeroCliente = document.getElementById("numerito");
 
 const divResultado = document.getElementById("resultadoCuadrado");
 
-
+/** funcion que calcula el cuadrado de un numero
+ * esto ese si hubiera params y return:
+ * @param {number} numeroCliente - el cliente entra un numero para calcular el cuadrado
+ * @return {number} - retorna el cuadrado
+ */
 function calcularCuadrado(){
     console.log(numeroCliente.value**2);
     divResultado.innerHTML = numeroCliente.value**2;
